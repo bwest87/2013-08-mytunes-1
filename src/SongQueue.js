e3b55def01ff28d56d8f809999dad9968cead976
Hack Reactor Students <github@hackreactor.com>
2013-08-21 16:28:41 -0700

    added playfirst method and working on event

--- src/SongQueue.js
+++ src/SongQueue.js
@@ -1,6 +1,8 @@
 var SongQueue = Songs.extend({
-
   initialize: function(){
+    this.on('add', this.playFirst, this);
+  },
+  playFirst: function() {
+    console.log("i'm getting called!");
   }
-
 });
