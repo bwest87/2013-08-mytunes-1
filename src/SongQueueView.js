824a34bad2f886eedbc8dc6920a633d2cd2513ff
Hack Reactor Students <github@hackreactor.com>
2013-08-21 20:08:35 -0700

    finished song queue view specs and player view specs

--- src/SongQueueView.js
+++ src/SongQueueView.js
@@ -1,10 +1,15 @@
 var SongQueueView = Backbone.View.extend({
+  tagName: 'table',
 
   initialize: function() {
+    this.collection.on('add', this.render, this);
+    this.collection.on('remove', this.render, this);
   },
 
   render: function() {
-    return this.$el;
+    return this.$el.html('<th>Queue</th>').append(this.collection.map(function(song) {
+      return new SongQueueEntryView({model: song}).render();
+    }));
   }
 
 });
