dfb41d80fd03f82d6c6c3c0410850a62a2281bda
Hack Reactor Students <github@hackreactor.com>
2013-08-22 17:10:39 -0700

    wrapped all of app in router

--- src/router.js
+++ src/router.js
@@ -3,7 +3,15 @@ var Router = Backbone.Router.extend({
     "testing" : "testing"
   },
   initialize: function(){
+    // set up model objects
+    var library = new Songs(songData);
+    var app = new App({library: library});
 
+    // build a view for the top level of the whole app
+    var appView = new AppView({model: app});
+
+    // put the view onto the screen
+    $('body').append(appView.render());
   },
 
   testing: function() {
