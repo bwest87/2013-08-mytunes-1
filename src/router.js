3adba72178378898bb94ead98ede4acaa2b763bc
Hack Reactor Students <github@hackreactor.com>
2013-08-22 17:30:46 -0700

    routes are working

--- src/router.js
+++ src/router.js
@@ -1,20 +1,24 @@
 var Router = Backbone.Router.extend({
   routes: {
-    "testing" : "testing"
+    "testing" : "testing",
+    ":title" : "playSongUrl"
   },
   initialize: function(){
     // set up model objects
-    var library = new Songs(songData);
-    var app = new App({library: library});
+    this.library = new Songs(songData);
+    this.app = new App({library: this.library});
 
     // build a view for the top level of the whole app
-    var appView = new AppView({model: app});
+    this.appView = new AppView({model: this.app});
 
     // put the view onto the screen
-    $('body').append(appView.render());
+    $('body').append(this.appView.render());
   },
 
   testing: function() {
     alert("I'm using routes!");
+  },
+  playSongUrl: function(title) {
+    this.library.where({'title' : title})[0].play();
   }
 });
